69c44c8fdae622c3f06f9e667099be0d5cb600c8
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2023-02-20 15:48:47 +0100

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'pages#ask'
   get 'ask', to: 'pages#ask'
   get 'answer', to: 'pages#answer'
   # Defines the root path route ("/")
